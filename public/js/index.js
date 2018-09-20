64b8766edcf6f0b23d9696cfd3b4896d785c28ab
rich2glad <rich2glad@gmail.com>
2018-09-20 15:53:37 -0400

    greet new user and alert new user

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,6 @@ var socket = io();
 
     socket.on('connect', function (){
         console.log('connected to server');
-
-    
-    // socket.emit('createMessage',{
-    //         to:'rich2@gmail.com',
-    //         text:'Hai how are youfrom rich2'
-    //         });
     });
 
     socket.on('disconnect',function(){
