e3ac00dd3c8f4ea11260fba9fd753ad12c63de7b
rich2glad <rich2glad@gmail.com>
2018-09-21 15:59:07 -0400

    Add form for messages and show inco,ming message to browser

--- public/js/index.js
+++ public/js/index.js
@@ -4,11 +4,37 @@ var socket = io();
         console.log('connected to server');
     });
 
+   
+
     socket.on('disconnect',function(){
           console.log('disconnected from server');
      });
 
 
      socket.on('newMessage', function(newMessage){
-        console.log('New Message',newMessage );
+        //console.log('New Message',newMessage );
+        var li = jQuery('<li></li>');
+        li.text(`${newMessage.from}: ${newMessage.text}`);
+        jQuery('#messages').append(li);
      });
+
+    //  socket.emit('createMessage',
+    //  {
+    //     from:'Gladius',
+    //     text:'Hi'
+    //  },function(data){
+
+    //      console.log('Got it',data);
+    //  });
+
+     jQuery('#message-form').on('submit',function(e){
+        e.preventDefault();
+        socket.emit('createMessage',{
+            from:'User',
+            text: jQuery('[name=message]').val()
+        },function(){
+
+        });
+     });
+
+    
