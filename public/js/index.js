75e361c22281fa915a8162b4250c18adfe9784c7
rich2glad <rich2glad@gmail.com>
2018-09-24 14:22:09 -0400

    auto scrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 var socket = io();
 
+    function scrollToBottom(){
+        var messages = jQuery('#messages');
+        var newMessage = messages.children('li:last-child');
+        var clientHeight = messages.prop('clientHeight');
+        var scrollTop = messages.prop('scrollTop');
+        var scrollHeight =messages.prop('scrollHeight');
+        var newMessageHeight = newMessage.innerHeight();
+        var lastMessageHeight= newMessage.prev().innerHeight();
+        if(clientHeight + scrollTop +newMessageHeight +lastMessageHeight>= 
+            scrollHeight){
+                messages.scrollTop(scrollHeight);
+        }
+    }
+
     socket.on('connect', function (){
         console.log('connected to server');
     });
@@ -20,7 +34,7 @@ var socket = io();
             createdAt:formattedTime
         });
         jQuery('#messages').append(html);
-        
+        scrollToBottom();
      });
 
      socket.on('newLocationMessage',function(message){
@@ -34,6 +48,7 @@ var socket = io();
             createdAt:formattedTime
         });
         jQuery('#messages').append(html);
+        scrollToBottom();
      });
 
 
